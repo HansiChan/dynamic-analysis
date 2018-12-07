84a445a4f102d16d5e292d0d3ac5eceb378c99ca
chenlg <chenlg@dachentech.com.cn>
2018-12-07 15:11:46 +0800

    fix

--- src/main/java/com/dachen/dynamicanalysis/dataprovider/ExcelExportProvider.java
+++ src/main/java/com/dachen/dynamicanalysis/dataprovider/ExcelExportProvider.java
@@ -57,7 +57,7 @@ public class ExcelExportProvider {
                 row.createCell(0).setCellValue(firstCell.get(i));
                 for (int j = 0; j < secondCell.size(); j++) {
                     row.createCell(0 + j)
-                            .setCellValue(secondCell.get(j)
+                            .setCellValue(secondCell.get(j).replace("\"","")
                                     .replace("[", "").replace("]", "").split(",")[i]);
                 }
 
